f761f1ec624230d3e235486effaa70e8cb20dbb7
trhieu2 <23020070@vnu.edu.vn>
2024-04-23 20:44:25 +0700

    map modifications

---
+++ src/Buttons/Button.h
@@ -0,0 +1,18 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "BaseObject.h"
+#include "TextManager.h"
+
+class Button
+{
+    public:
+        Button();
+        virtual ~Button();
+
+    protected:
+
+    private:
+};
+
+#endif // BUTTON_H
