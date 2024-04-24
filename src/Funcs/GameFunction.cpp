50a9c566ff8742d20db64825a7e4efb73c82b540
trhieu2 <23020070@vnu.edu.vn>
2024-04-24 10:53:16 +0700

    optimized the collison detection

--- src/Funcs/GameFunction.cpp
+++ src/Funcs/GameFunction.cpp
@@ -1,6 +1,8 @@
 #include "GameFunction.h"
 #include "TextManager.h"
 
+#include <math.h>
+
 GameFunction::GameFunction()
 {
     //ctor
@@ -13,88 +15,33 @@ GameFunction::~GameFunction()
 
 bool SDLCommonFunc::CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2)
 {
-  int left_a = object1.x;
-  int right_a = object1.x + object1.w;
-  int top_a = object1.y;
-  int bottom_a = object1.y + object1.h;
+    // Calculate the center coordinates of each object
+    int centerX1 = object1.x + object1.w / 2;
+    int centerY1 = object1.y + object1.h / 2;
+    int centerX2 = object2.x + object2.w / 2;
+    int centerY2 = object2.y + object2.h / 2;
 
-  int left_b = object2.x;
-  int right_b = object2.x + object2.w;
-  int top_b = object2.y;
-  int bottom_b = object2.y + object2.h;
+    // Calculate the distance between the centers of the objects
+    int distance = sqrt(pow(centerX2 - centerX1, 2) + pow(centerY2 - centerY1, 2));
 
-  // Case 1: size object 1 < size object 2
-  if (left_a > left_b && left_a < right_b)
-  {
-    if (top_a > top_b && top_a < bottom_b)
+    // Calculate the size of the collision box for each object (half of the object's width)
+    int box1Size = object1.w / 2;
+    int box2Size = object2.w / 2;
+
+    // Calculate the minimum distance for collision (sum of the radii)
+    int minDistance = (box1Size + box2Size) / 2;
+
+    // Check if the distance is less than the minimum distance for collision
+    if (distance < minDistance)
     {
-      return true;
+        // Collision detected
+        return true;
     }
-  }
-
-  if (left_a > left_b && left_a < right_b)
-  {
-    if (bottom_a > top_b && bottom_a < bottom_b)
+    else
     {
-      return true;
+        // No collision
+        return false;
     }
-  }
-
-  if (right_a > left_b && right_a < right_b)
-  {
-    if (top_a > top_b && top_a < bottom_b)
-    {
-      return true;
-    }
-  }
-
-  if (right_a > left_b && right_a < right_b)
-  {
-    if (bottom_a > top_b && bottom_a < bottom_b)
-    {
-      return true;
-    }
-  }
-
-  // Case 2: size object 1 < size object 2
-  if (left_b > left_a && left_b < right_a)
-  {
-    if (top_b > top_a && top_b < bottom_a)
-    {
-      return true;
-    }
-  }
-
-  if (left_b > left_a && left_b < right_a)
-  {
-    if (bottom_b > top_a && bottom_b < bottom_a)
-    {
-      return true;
-    }
-  }
-
-  if (right_b > left_a && right_b < right_a)
-  {
-    if (top_b > top_a && top_b < bottom_a)
-    {
-      return true;
-    }
-  }
-
-  if (right_b > left_a && right_b < right_a)
-  {
-    if (bottom_b > top_a && bottom_b < bottom_a)
-    {
-      return true;
-    }
-  }
-
-   // Case 3: size object 1 = size object 2
-  if (top_a == top_b && right_a == right_b && bottom_a == bottom_b)
-  {
-    return true;
-  }
-
-  return false;
 }
+
 
